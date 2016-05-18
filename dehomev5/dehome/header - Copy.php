cd793a94a74af209f675fd1463b35a3b3dec69a1
Tran Tea <Tran Tea>
2016-05-18 23:29:50 +0700

    PM 18-05-2016

--- dehomev5/dehome/header - Copy.php
+++ dehomev5/dehome/header - Copy.php
@@ -70,76 +70,6 @@
           </div><!-- header-top -->
         </div><!-- row -->
         <div class="row">
-          <div class="main-menu">
-            <nav class="navbar navbar-default" role="navigation">
-              <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="#">Menu</a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav nav-pills nav-justified">
-                    <li class="dropdown active">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tìm Photo<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Nhà Phố</a></li>
-                        <li><a href="#">Biệt Thự</a></li>
-                        <li><a href="#">Nội Thất</a></li>
-                        <li><a href="#">Phòng Khách</a></li>
-                        <li><a href="#">Bếp</a></li>
-                        <li><a href="#">Phòng Ngủ</a></li>
-                        <li><a href="#">Phòng Bé Trai</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown active">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tìm Chuyên Gia<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Thiết Kế & Thi Công</a></li>
-                        <li><a href="#">Thầu Xây Thô</a></li>
-                        <li><a href="#">Thầu Nội Thất</a></li>
-                        <li><a href="#">Thi Công Cảnh Quan</a></li>
-                        <li><a href="#">Nhà Thầu Điện Lạnh</a></li>
-                        <li><a href="#">Tất Cả Nhà Thầu</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown active">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Showroom<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Trang Trí Nội Thất</a></li>
-                        <li><a href="#">Vật Liệu Hoàn Thiện</a></li>
-                        <li><a href="#">Vật Liệu Thô</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown active">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Bài Viết<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Kiến Trúc</a></li>
-                        <li><a href="#">Nội Thất</a></li>
-                        <li><a href="#">Trang Trí</a></li>
-                        <li><a href="#">Cảnh Quan</a></li>
-                        <li><a href="#">Xây Dựng</a></li>
-                        <li><a href="#">Vật Liệu</a></li>
-                      </ul>
-                    </li>
-                    <li class="dropdown active">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gốc Tư Vấn<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Câu Hỏi Thường Gặp</a></li>
-                        <li><a href="#">Hỏi Chuyên Gia</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div><!-- /.navbar-collapse -->
-              </div><!-- /.container-fluid -->
-            </nav>
-          </div>  
           <div class="header-bottom clearfix">
             <div class="col-sm-2 header-bottom-col header-bottom-col-1">
               <div class="header-left-menu dropdown clearfix">
@@ -149,11 +79,50 @@
                     <span class="caret"></span>
                   </button>
                   <ul class="dropdown-menu" aria-labelledby="header-left-menu">
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-                    <li role="presentation" class="divider"></li>
-                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                    <li><a href="#">Tìm Photo</a>
+                      <ul class="sub-menu">
+                          <li><a href="#">Nhà Phố</a></li>
+                          <li><a href="#">Biệt Thự</a></li>
+                          <li><a href="#">Nội Thất</a></li>
+                          <li><a href="#">Phòng Khách</a></li>
+                          <li><a href="#">Bếp</a></li>
+                          <li><a href="#">Phòng Ngủ</a></li>
+                          <li><a href="#">Phòng Bé Trai</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Tìm Chuyên Gia</a>
+                      <ul class="sub-menu">
+                          <li><a href="#">Thiết Kế & Thi Công</a></li>
+                          <li><a href="#">Thầu Xây Thô</a></li>
+                          <li><a href="#">Thầu Nội Thất</a></li>
+                          <li><a href="#">Thi Công Cảnh Quan</a></li>
+                          <li><a href="#">Nhà Thầu Điện Lạnh</a></li>
+                          <li><a href="#">Tất Cả Nhà Thầu</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Showroom</a>
+                      <ul class="sub-menu">
+                          <li><a href="#">Trang Trí Nội Thất</a></li>
+                          <li><a href="#">TVật Liệu Hoàn Thiện</a></li>
+                          <li><a href="#">Vật Liệu Thô</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Bài Viết</a>
+                      <ul class="sub-menu">
+                          <li><a href="#">Kiến Trúc</a></li>
+                          <li><a href="#">Nội Thất</a></li>
+                          <li><a href="#">Trang Trí</a></li>
+                          <li><a href="#">Cảnh Quan</a></li>
+                          <li><a href="#">Xây Dựng</a></li>
+                          <li><a href="#">Vật Liệu</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Gốc Tư Vấn</a>
+                      <ul class="sub-menu">
+                        <li><a href="#">Câu Hỏi Thường Gặp</a></li>
+                        <li><a href="#">Hỏi Chuyên Gia</a></li>
+                      </ul>
+                    </li>
                     <!-- <li role="separator" class="divider"></li>
                     <li><a href="#">Separated link</a></li> -->
                   </ul>
