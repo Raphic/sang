ba6975b1a0516204fc9a69a43926a4dc27f131a6
Tran Tea <Tran Tea>
2016-05-25 23:45:06 +0700

    PM 25-05-2016

---
+++ Fundraising/mail2.php
@@ -0,0 +1,85 @@
+				<?php
+				    include 'include/header_mail.php';
+				?>
+				<div class="main flex">
+					<div>
+						<div class="left">
+							<img src="img/mail_main.jpg">
+							<p>We're raising money for new outdoor planters.</p>
+						</div>
+						<div class="bottom_mail2">
+							<p>I want to make an additional pledge amount to increase my total.</p>
+							<a class="button" href="#">Make Additional Pledge</a>
+							<img src="img/shadow_mail2.png">
+						</div>
+					</div>
+					
+
+					<div class="right">
+						<div class="top flex">
+							<p>Dear Uncle Bill,<br>
+								On this page you have the option to recieve XX Free PledgeBids to use in the schools on line penny auctions. There's no cost to you and you can win some great items.</p>	
+							<img src="img/mail_avatar.jpg">
+						</div>
+						<div class="bottom flex">
+							<div class="main_mail2">
+								<input type="radio" name="" value="" checked>Yes, I want to receive my Free pledgebids to use in the schools penny auctions.
+								<form>
+									<p>Create your bidding profile:</p>
+									<table>
+										<tr>
+											<td>Enter a screen/user name</td>
+											<td><input type="text" name="" value="" placeholder="Ex: Bill Jones"></td>
+										</tr>
+										<tr>
+											<td>Enter a Password</td>
+											<td><input type="password" name="" value="" placeholder="Ex: Bill Jones"></td>
+										</tr>
+										<tr>
+											<td>Enter a screen/user name</td>
+											<td><input type="text" name="" value="" placeholder="Ex: Bill Jones"></td>
+										</tr>
+									</table>
+								</form>
+								<input type="radio" name="" value="">No, I do not want my Free pledgebids..
+								<div class="congratulations">
+									<p>Congratulations !  You are in the top ten donor list. Please provide the screen name you would like displayed.  This will be viewable participating in this fundraiser.</p>
+									<form>
+										<input type="text" name="" value="" placeholder="Enter your screen name here"><span>or</span>
+										<div>
+											<input type="checkbox" name="" value="">
+											<p>Please use "Annonomous" for our pledge amount</p>
+										</div>
+										<textarea name="" value="" placeholder="Please enter your words of encouragement here. Monica will see your message on her family page."></textarea>
+										<div class="border_button">
+											<a class="button"href="">Submit</a>	
+											<img src="img/shadow_submit.png">
+										</div>																																"
+									</form>
+									
+								</div>	
+							</div>
+							<div class="salary">
+								<ul>
+									<li>Smith Family  $150</li>
+									<li>Anonomous  $75</li>
+ 									<li>Gregory Family $50</li>
+ 									<li>Jim Smith  $45</li>
+ 									<li>Jim Smith  $45</li>
+ 									<li>Willy's Car Wash $40</li>
+ 									<li>Uncle Ben  $35</li>
+ 									<li>Grandma $30 </li>
+ 									<li>Auntie Emma $25</li>
+ 									<li>Anonomous  $20</li>
+ 									<li>Anonomous  $15</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php
+	    include 'include/footer.php';
+	?>
