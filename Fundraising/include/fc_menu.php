81520e3167ec0caf52b4e88fe1544f081d1319a3
Dang Khoa <tndkhoa1104@gmail.com>
2016-05-27 17:13:33 +0700

    PM 27-05-2016

---
+++ Fundraising/include/fc_menu.php
@@ -0,0 +1,14 @@
+<div class="home_menu fc_menu">
+    <ul>
+        <li><a href="#">DashBoard</a></li>
+        <li class="active"><a href="HomeFundraisingTypes.php">Download Forms</a></li>
+        <li><a href="#">Reports</a></li>
+        <li><a href="#">Pledge Sheet Entry</a></li>
+        <li><a href="#">Reminder Notices</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</div>
+
+
+
+
